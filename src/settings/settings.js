4a31f67a594931c3b655a428dfe5b8fab2023dbc
Noor Taquee <taquee1416@gmail.com>
2026-02-01 00:18:58 +0530

    new file structure

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -1,148 +1,14 @@
-class element {
-  constructor() {
-    this.name = "";
-    this.symbol = "";
-    this.atomic_number = "";
-    this.electronic_configuration = "";
-    this.period = "";
-    this.group = "";
-    this.block = "";
-    this.category = "";
+import { createElement } from "../utils/UI/create-dom.js";
+import { app } from "../main.js";
+import { updateElementState, table } from "../main.js";
 
-    this.mass = "";
-    this.density = "";
-    this.phase = "";
-    this.boiling = "";
-    this.melting = "";
-    this.electronegativity = "";
-    this.atomic_radius = "";
-    this.year_discovered = "";
-  }
-}
-
-let allElements;
-let focused_element;
-
-function createElement(atomic_number, element) {
-  let elementBtn = document.createElement("button");
-  elementBtn.id = element.symbol;
-
-  elementBtn.classList.add("element-button");
-  elementBtn.classList.add(`group-${element.group}`);
-  elementBtn.classList.add(`period-${element.period}`);
-  elementBtn.classList.add(`block-${element.block}`);
-  elementBtn.classList.add(`state-${element.phase}`.toLowerCase());
-  elementBtn.classList.add(`category-${element.category.replace(/ /g, '-')}`.toLowerCase());
-
-  let div1 = document.createElement("div");
-  div1.className = "element-info-div";
-  // Atomic number
-  let atomic_no_p = document.createElement("p");
-  atomic_no_p.textContent = atomic_number;
-  atomic_no_p.className = "atomic-number";
-  div1.appendChild(atomic_no_p);
-  elementBtn.appendChild(div1);
-
-  let div2 = document.createElement("div");
-  div2.className = "element-name-div";
-  // Symbol
-  let symbol_p = document.createElement("p");
-  symbol_p.textContent = element.symbol;
-  symbol_p.className = "element-symbol";
-  div2.appendChild(symbol_p);
-  // Name
-  let name_p = document.createElement("p");
-  name_p.textContent = element.name;
-  name_p.className = "element-name";
-  div2.appendChild(name_p);
-  elementBtn.appendChild(div2);
-
-  elementBtn.addEventListener("click", () => showInfo(atomic_number, element));
-
-  return elementBtn;
-}
-
-function createTable() {
-  for (let atomic_number in allElements) {
-    let el = allElements[atomic_number];
-    let parent;
-    if (el.block == "f") {
-      if (atomic_number > 88) {
-        parent = document.getElementsByClassName(`period 7`)[0];
-      } else {
-        parent = document.getElementsByClassName(`period 6`)[0];
-      }
-    } else {
-      parent = document.getElementsByClassName(`group ${el.group}`)[0];
-    }
-
-    parent.appendChild(createElement(atomic_number, el));
-  }
-}
-
-async function loadElements() {
-  const response = await fetch("elementsData.json");
-  const data = await response.json();
-  allElements = data;
-}
-
-function showInfo(atomic_number, element) {
-  if (infoPanel.style.display == "none") { infoPanel.style.display = "flex" };
-  infoPanel.scrollIntoView();
-  // header elements
-  document.getElementById("atomic-number").textContent = atomic_number;
-  document.getElementById("element-name").textContent = element.name;
-  document.getElementById("electronic-configuration").textContent = element.electronic_configuration;
-  // body elements
-  document.getElementById("element-symbol-text").textContent = element.symbol;
-  document.getElementById("atomic-radius-text").textContent = element.atomic_radius;
-  document.getElementById("mass-number-text").textContent = `${element.mass} u`;
-  document.getElementById("density-text").textContent = element.density;
-  document.getElementById("electronegativity-text").textContent = element.electronegativity;
-  document.getElementById("phase-text").textContent = element.phase;
-  document.getElementById("period-text").textContent = element.period;
-  document.getElementById("group-text").textContent = element.group;
-  document.getElementById("block-text").textContent = element.block;
-  document.getElementById("category-text").textContent = element.category;
-  document.getElementById("year-discovered-text").textContent = element.year_discovered;
-  document.getElementById("melting-point-text").textContent = element.melting;
-  document.getElementById("boiling-point-text").textContent = element.boiling;
-  document.getElementById("learn-more-link").href = `https://en.wikipedia.org/wiki/${element.name}`;
-}
-
-function updateElementState() {
-  for (let atomic_number in allElements) {
-    let element = allElements[atomic_number];
-    let elementBtn = document.getElementById(element.symbol);
-
-    // remove existing state
-    elementBtn.classList.remove("state-solid");
-    elementBtn.classList.remove("state-liquid");
-    elementBtn.classList.remove("state-gas");
-
-    // add calculated state
-    if (current_temperature < element.melting) {
-      elementBtn.classList.add("state-solid");
-    } else if (current_temperature < element.boiling) {
-      elementBtn.classList.add("state-liquid");
-    } else {
-      elementBtn.classList.add("state-gas");
-    }
-  }
-}
-
-const app = document.getElementById("app");
-
-const table = document.getElementById("periodic-table");
-const infoPanel = document.getElementById("info-panel");
-
-// #region control panel
+//#region control panel
 const controlPanel = document.getElementById("control-panel");
 controlPanel.addEventListener("animationend", () => { controlPanel.style.animation = 'none' });
 
 const controlResizeHandle = document.getElementById("control-resize-handle");
 
-const controlNavigation = document.getElementById("control-navigation");
+// const controlNavigation = document.getElementById("control-navigation");
 const controlNavIndicator = document.getElementById("control-nav-indicator");
 
 const controlPanelTabContainer = document.getElementById("control-panel-tab-container");
@@ -152,11 +18,12 @@ const appearanceControlsPanel = document.getElementById("appearance-controls-pan
 
 const controlShowBtn = document.getElementById("control-show-btn");
 controlShowBtn.addEventListener("click", showControlPanel);
+
 function showControlPanel() {
   if (controlPanel.style.display == "flex") return;
 
   controlPanel.style.display = "flex";
-  controlPanel.style.animation = (app.classList.contains("vertical")) ? "slide-in-bottom 0.3s ease" : "slide-in-right 0.3s ease";
+  controlPanel.style.animation = (app.dataset.orientation == "vertical") ? "slide-in-bottom 0.3s ease" : "slide-in-right 0.3s ease";
 }
 
 const controlHideBtn = document.getElementById("control-hide-btn");
@@ -164,20 +31,20 @@ controlHideBtn.addEventListener("click", hideControlPanel);
 function hideControlPanel() {
   if (controlPanel.style.display == "none") return;
 
-  controlPanel.style.animation = (app.classList.contains("vertical")) ? "slide-out-bottom 0.3s ease" : "slide-out-right 0.3s ease";
+  controlPanel.style.animation = (app.dataset.orientation == "vertical") ? "slide-out-bottom 0.3s ease" : "slide-out-right 0.3s ease";
   controlPanel.addEventListener("animationend", () => { controlPanel.style.display = 'none' }, { once: true });
 }
 
-document.querySelectorAll("#control-panel-tab-container .control-slider").forEach((slider) => {
+controlPanelTabContainer.querySelectorAll(".control-slider").forEach((slider) => {
   slider.addEventListener("touchstart", () => { controlPanelTabContainer.style.overflowX = "hidden" });
   slider.addEventListener("touchend", () => { controlPanelTabContainer.style.overflowX = "scroll" });
 });
 
-// #region control resize
+//#region control resize
 function handleControlResize(event) {
 
   // Vertical resizing
-  if (app.classList.contains("vertical")) {
+  if (app.dataset.orientation == "vertical") {
     const dY = window.innerHeight - event.clientY;
 
     // If the panel becomes too short, hide it
@@ -214,9 +81,9 @@ controlResizeHandle.addEventListener("pointerup", (event) => {
   controlResizeHandle.releasePointerCapture(event.pointerId);
   controlResizeHandle.removeEventListener("pointermove", handleControlResize);
 });
-// #endregion control resize
+//#endregion control resize
 
-// #region control navigation
+//#region control navigation
 const elementControlsBtn = document.getElementById("element-controls-btn");
 elementControlsBtn.addEventListener("click", () => {
   elementControlsPanel.scrollIntoView();
@@ -261,17 +128,20 @@ function isElementCentered(element, scrollParent) {
 
   return Math.abs(containerCenter - elementCenter) <= tolerance;
 }
-// #endregion control navigation
+//#endregion control navigation
 
-// #region element controls
-let viewModeSelect = document.getElementById("view-mode-dropdown");
+//#region element controls
+const viewModeSelect = document.getElementById("view-mode-dropdown");
+window.addEventListener("DOMContentLoaded", changeViewMode);
 viewModeSelect.addEventListener("change", changeViewMode);
 
 function changeViewMode() {
   table.classList.remove("by-state");
   table.classList.remove("by-block");
   table.classList.remove("by-category");
+  
   table.classList.add(viewModeSelect.value);
+  
   document.querySelectorAll(".color-relevance-div").forEach((div) => {
     div.style.display = "none";
   });
@@ -284,23 +154,23 @@ let current_temperature = 298;
 
 const temperature_input = document.getElementById("temperature-control-value");
 temperature_input.addEventListener("input", () => {
-  // Update temperature value to show current temperature
   current_temperature = temperature_input.value;
+  // Update temperature value to show current temperature
   temperature_slider.value = current_temperature;
   updateElementState();
 });
 
 const temperature_slider = document.getElementById("temperature-slider");
 temperature_slider.addEventListener("input", () => {
-  // Update temperature value to show current temperature
   current_temperature = temperature_slider.value;
+  // Update temperature value to show current temperature
   temperature_input.value = current_temperature;
   updateElementState();
 });
 
-// #endregion element controls
+//#endregion element controls
 
-// #region appearance controls
+//#region appearance controls
 
 function updateDeviceColor() {
   const themeTag = document.querySelector('meta[name="theme-color"]');
@@ -308,61 +178,63 @@ function updateDeviceColor() {
   themeTag.setAttribute("content", color);
 }
 
-// #region theme
-function chooseTheme(theme, button) {
-  if (app.classList.contains(theme)) return;
+//#region theme
+// depends [ app, paletteSection, updateDeviceColor]
 
-  document.querySelectorAll(".palette-input").forEach((input) => {
-    input.classList.remove(selectedTheme);
-    input.classList.add(theme);
+/**
+ * @param {string} theme 
+ * @param {HTMLElement} button 
+ * @returns 
+ */
+function chooseTheme(theme, button) {
+  if (app.dataset.theme == theme) return;
+
+  paletteSelection.querySelectorAll(".palette-input").forEach((input) => {
+    input.dataset.theme = theme;
   });
 
-  app.classList.remove(selectedTheme);
-  selectedThemeIcon.classList.remove("selected");
+  themeSelection.querySelectorAll(".selected").forEach(selectedThemeIcon => {
+    selectedThemeIcon.classList.remove("selected");
+  });
 
-  selectedTheme = theme;
-  selectedThemeIcon = button;
-
-  app.classList.add(selectedTheme);
-  selectedThemeIcon.classList.add("selected");
+  app.dataset.theme = theme;
+  button.classList.add("selected");
 
   updateDeviceColor();
 }
 
-let selectedTheme;
-let selectedThemeIcon;
+const themeSelection = document.getElementById("theme-control-div");
 
 const lightBtn = document.getElementById("light-btn");
-selectedTheme = "light";
-selectedThemeIcon = lightBtn;
 lightBtn.addEventListener("click", () => { chooseTheme("light", lightBtn) });
 
 const darkBtn = document.getElementById("dark-btn");
 darkBtn.addEventListener("click", () => { chooseTheme("dark", darkBtn) });
-// #endregion theme
+//#endregion theme
 
-// #region palette
+//#region palette
+// depends [ app, updateDeviceColor ]
+/**
+ * @param {string} palette 
+ * @param {HTMLElement} button 
+ * @returns 
+ */
 function choosePalette(palette, button) {
-  if (app.classList.contains(palette)) return;
+  if (app.dataset.palette == palette) return;
 
-  app.classList.remove(selectedPalette);
-  selectedPaletteIcon.classList.remove("selected");
+  paletteSelection.querySelectorAll(".selected").forEach(selectedPaletteIcon => {
+    selectedPaletteIcon.classList.remove("selected");
+  });
 
-  selectedPalette = palette;
-  selectedPaletteIcon = button;
+  app.dataset.palette = palette;
+  button.classList.add("selected");
 
-  app.classList.add(selectedPalette);
-  selectedPaletteIcon.classList.add("selected");
-  
   updateDeviceColor();
 }
 
-let selectedPalette;
-let selectedPaletteIcon;
+const paletteSelection = document.getElementById("palette-control-div");
 
 const blueBtn = document.getElementById("blue-btn");
-selectedPalette = "blue";
-selectedPaletteIcon = blueBtn;
 blueBtn.addEventListener("click", () => { choosePalette("blue", blueBtn) });
 
 const limeBtn = document.getElementById("lime-btn");
@@ -373,8 +245,11 @@ pinkBtn.addEventListener("click", () => { choosePalette("pink", pinkBtn) });
 
 const purpleBtn = document.getElementById("purple-btn");
 purpleBtn.addEventListener("click", () => { choosePalette("purple", purpleBtn) });
-// #endregion palette
+//#endregion palette
 
+
+//#region sliders
+// depends [table]
 const elementSize_slider = document.getElementById("element-size-slider");
 elementSize_slider.addEventListener("input", () => {
   table.style.setProperty("--element-size-scale", elementSize_slider.value);
@@ -394,28 +269,7 @@ const elementName_slider = document.getElementById("element-name-slider");
 elementName_slider.addEventListener("input", () => {
   table.style.setProperty("--element-name-scale", elementName_slider.value);
 });
-// #endregion appearance controls
-// #endregion control panel
+//#endregion sliders
 
-
-function checkOrientation() {
-  if (document.body.offsetHeight > document.body.offsetWidth) {
-    app.classList.remove("horizontal");
-    app.classList.add("vertical");
-  } else {
-    app.classList.remove("vertical");
-    app.classList.add("horizontal");
-  }
-}
-
-window.addEventListener("resize", () => {
-  checkOrientation();
-});
-
-window.addEventListener("DOMContentLoaded", () => {
-  checkOrientation();
-  loadElements().then(() => {
-    createTable();
-  });
-  changeViewMode();
-});
+//#endregion appearance controls
+//#endregion control panel
