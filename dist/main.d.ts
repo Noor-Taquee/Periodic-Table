bcdac4c84e4be95b9692bc72bf45985289609ba9
Noor Taquee <noortaquee001@gmail.com>
2026-05-23 07:28:12 +0530

    added node_modules and dist to gitignore

--- dist/main.d.ts
+++
@@ -1,5 +0,0 @@
-export declare let allElements: any;
-export declare const app: HTMLDivElement;
-export declare const table: HTMLDivElement;
-export declare const infoPanel: HTMLDivElement;
-//# sourceMappingURL=main.d.ts.map
--- dist/settings/settings.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function updateElementState(): void;
-//# sourceMappingURL=settings.d.ts.map
